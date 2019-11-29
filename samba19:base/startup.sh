c9595742a96f28e77de9b197e9dd8abe3fc9cd29
Eduard Canet <ecanet@localhost.localdomain>
2019-11-29 09:50:37 +0100

    samba

--- samba19:base/startup.sh
+++ samba19:base/startup.sh
@@ -6,5 +6,16 @@
 /opt/docker/install.sh && echo "Install Ok"
 /usr/sbin/smbd && echo "smb Ok"
 /usr/sbin/nmbd && echo "nmb  Ok"
+
+# Creació usuaris unix/samba
+useradd lila
+useradd roc
+useradd patipla
+useradd pla
+echo -e "lila\nlila" | smbpasswd -a lila
+echo -e "roc\nroc" | smbpasswd -a roc
+echo -e "patipla\npatipla" | smbpasswd -a patipla
+echo -e "pla\npla" | smbpasswd -a pla
+
 /bin/bash
 
